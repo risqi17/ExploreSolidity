b851d35614c3cb0228215be5511cb16c7f99766c
risqi17 <ahmadreys@gmail.com>
2025-01-16 16:54:20 +0700

    contract

--- contracts/MarketplaceBuku.sol
+++ contracts/MarketplaceBuku.sol
@@ -1,4 +1,16 @@
 // SPDX-License-Identifier= MIT
 
-pragma solidity >=0.8.2 <0.9.0
+pragma solidity >=0.8.2 <0.9.0;
 
+contract marketplaceBuku {
+    struct Buku {
+        string namaBuku;
+        uint hargaBuku;
+    }
+
+    Buku[] public semuaBuku;
+
+    function tambahBuku(string _namaBuku, uint _hargaBuku) public {
+        semuaBuku.
+    }
+}
